9f86b12a826f8d90e90eb1dfcfa2b02912d47948
Harrison <33430956+HarStu@users.noreply.github.com>
2025-07-04 12:02:35 -0400

    Default to a single erambank to pass tests

--- src/mem.js
+++ src/mem.js
@@ -5,13 +5,14 @@ export class Mem {
   constructor() {
     this.romBanks = []; // ROM banks (Bank 0 fixed, others switchable)
     this.romBanks[0] = new Uint8Array(0x4000); // 16KB initial fixed ROM bank
-    this.romBanks[1] = new Uint8Array(0x4000);
+    this.romBanks[1] = new Uint8Array(0x4000); // 16KB default swappable bank
     this.crb = 1; // Current ROM bank. Set to 1 by default (0 contains the 0x0000-0x3FFF fixed ROM bank)
 
     this.vram = new Uint8Array(0x2000); // 8KB Video RAM, located 0x8000-0x9FFF
 
     this.eramBanks = []; // External RAM banks
-    this.cerb = -1; // Current eram bank. Defaults to -1 (no eram banks on cart)
+    this.cerb = 0; // Current eram bank. Defaults to 1 (one eram bank on cart)
+    this.eramBanks[0] == new Uint8Array(0x2000);
 
     this.wram = new Uint8Array(0x2000); // 8KB Work RAM, located 0xC000-0xDFFF
     this.oam = new Uint8Array(0x9F); // Object Attribute Memory
