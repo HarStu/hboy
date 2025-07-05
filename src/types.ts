116ad81dc95730a4f765dddcb45dd7eef0f71a89
Harrison <33430956+HarStu@users.noreply.github.com>
2025-07-05 18:10:32 -0400

    Begin moving CPU to typescript

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 // Basic number sizes for clarity
-export type Byte = number & { __type: 'byte' };
-export type Word = number & { __type: 'word' };
+export type Byte = number;
+export type Word = number;
 
 // 8-bit registers
 export type R8 = 'a' | 'b' | 'c' | 'd' | 'e' | 'h' | 'l' | 'f';
@@ -12,4 +12,4 @@ export type R16 = 'af' | 'bc' | 'de' | 'hl' | 'sp' | 'pc';
 export type Reg = R8 | R16;
 
 // Flags 
-export type flag = 'z' | 'n' | 'h' | 'c';
+export type Flag = 'z' | 'n' | 'h' | 'c';
